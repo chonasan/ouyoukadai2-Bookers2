210c793805378969a46f097640939a5b3cc7d997
chonasan <tk24120824@gmail.com>
2021-03-01 14:28:32 +0000

    コメント

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,53 +2,59 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def index
+    
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-
-  def create
-    @user = current_user
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+  
+  def edit
+     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render action: :edit
     else
-      @books = Book.all
-      render action: :index
+      redirect_to books_path
     end
   end
+  
 
-  def edit
-    @book = Book.find(params[:id])
+  def create
+      @user = current_user
+      @book = Book.new(book_params) #Book.newの:title, :bodyを更新する
+      @book.user_id = current_user.id #nillのidを更新
+      @books = Book.all
+     if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+     else
+      render action: :index
+     end
   end
-
-
-
+  
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+     flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
     else
       render action: :edit
     end
   end
-
-  def delete
+  
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path  
   end
-
+  
   private
-
-  def book_params
-    params.require(:book).permit(:title,:body,:profile_image)
-  end
-
+   def book_params
+    params.require(:book).permit(:title, :body, :profile_image)
+   end
+  
 end
